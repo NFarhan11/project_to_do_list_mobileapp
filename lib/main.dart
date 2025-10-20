81267461ad905ceb5be4fb1560b5883efbb1eaf3
NFarhan11 <najmifarhan9903@gmail.com>
2025-10-20 08:52:53 +0800

    added new page Add New Task

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -26,11 +35,27 @@ class MainApp extends StatelessWidget {
 
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('FAB pressed');
+            // print('FAB pressed');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddTaskPage()),
+            );
           },
           child: Icon(Icons.add),
         ),
       ),
+    );
+  }
+}
+
+class AddTaskPage extends StatelessWidget {
+  const AddTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add New Task'), backgroundColor: Colors.blue),
+      body: Center(child: Text('New Task Page')),
     );
   }
 }
